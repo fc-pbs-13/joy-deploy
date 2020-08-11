f855c092ad4a6938b86fdd5001e219ecbe1657a7
김주원 <kjw11077@naver.com>
2020-08-11 14:57:27 +0900

    whitenoise

--- deploy1/deploy1/settings/base.py
+++ deploy1/deploy1/settings/base.py
@@ -27,9 +27,9 @@ environ.Env.read_env(env_file=env)
 SECRET_KEY = 'n2v3*+8bc#y(p_6*84x19r#+9oqcz$b7!ottey&b!pprqofktm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
